d8fa88a827639cfcc149ad50246d94de9afe0104
catiemondon <Cathmond25@gmail.com>
2019-04-30 15:37:49 -0600

    updated image url to myimage

--- app-5/src/Image.js
+++ app-5/src/Image.js
@@ -4,7 +4,7 @@ class Image extends Component{
     render(){
         return(
             <div>
-                <img src={this.props.url} alt='gagging gal' />
+                <img src={this.props.myImage} alt='gagging gal' />
                 <p>Sorry that my gifs and funny memes wont load Matt :( I tried to amuse you as you were grading these.</p>
             </div>
         )
