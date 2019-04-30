1844ae2b48d36d40bb4ffeb2f064ecda4159f68a
catiemondon <Cathmond25@gmail.com>
2019-04-30 15:27:35 -0600

    did app-5

---
+++ app-5/src/Image.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Image extends Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.url} alt='gagging gal' />
+                <p>Sorry that my gifs and funny memes wont load Matt :( I tried to amuse you as you were grading these.</p>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default Image
